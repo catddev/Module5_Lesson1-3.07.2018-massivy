0008bcbf95d1554d852ca0dcdc870dcfbe17e1d6
айкеновад <@234_11>
2018-07-03 10:53:17 +0600

    -

--- Module5_Lesson1 (3.07.2018)/Source.cpp
+++ Module5_Lesson1 (3.07.2018)/Source.cpp
@@ -26,12 +26,38 @@ start:
 	{
 	case 1:
 	{
+		/*int a[10];
+		
+		const int n = 10;
+		int a[] = { 12, 30, 9 };
 
+		for (int i = 0; i < 10; i++)
+		{
+			cout << a[i] << endl;
+		}
+		*/
 	}
 	break;
 	case 2:
 	{
+		const int n = 5;
+		int a[n];
 
+		int max_v = INT_MIN; // когда ищем максимум вводим сначала МИНИМАЛЬНО возможное значение, потом с ним сравниваем
+		int min_v = INT_MAX; // здесь наоборот ищем минимум и сравниваем с максимальным значением
+
+		/*int max = a[1];*/
+
+		for (int i = 0; i < 5; i++)
+		{
+			cin >> a[i];
+			if (a[i] > max_v) max_v = a[i];
+			if (a[i] < min_v) min_v = a[i];
+		}
+		cout << max_v << endl << min_v << endl;
+		
+
+		
 	}
 	break;
 	case 3:
