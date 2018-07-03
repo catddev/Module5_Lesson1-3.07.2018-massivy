cca225a7e876a6c2055731b4edeef1b051d8755a
айкеновад <@234_11>
2018-07-03 11:32:26 +0600

    -

--- Module5_Lesson1 (3.07.2018)/Source.cpp
+++ Module5_Lesson1 (3.07.2018)/Source.cpp
@@ -54,25 +54,73 @@ start:
 			if (a[i] > max_v) max_v = a[i];
 			if (a[i] < min_v) min_v = a[i];
 		}
-		cout << max_v << endl << min_v << endl;
-		
-
+		cout << "максимальное значение: "<< max_v << endl << "минимальное значение: "<< min_v << endl;
 		
 	}
 	break;
-	case 3:
+	case 3: // проверка симметричности
 	{
+
+		const int n = 6;
+		int a[n];
+		bool flag = true;
+
+		for (int i = 0; i < n; i++)
+		{
+			cin >> a[i];
+
+			if (a[i] == a[n - i - 1]) flag = true;
+			else flag = false;
+			if (i > n / 2+1) break;
+		}
+		if (flag) cout << "symmetric" << endl;
+		else cout << "non-symmetic" << endl;
+
 
 	}
 	break;
-	case 4:
+	case 4: //проверка на симметричность массива - оптимальное решение
 	{
+		const int n = 10;
+		int a[n] = {1, 2, 3, 4, 5, 5, 4, 7, 2, 1};
+		bool f = true;
 
+		for (int i = 0; i < n/2; i++)
+		{
+			if (a[i] != a[n - 1 - i])
+			{
+				f = false;
+				break;
+			}
+		}
+		if(f) cout << "symmetric" << endl;
+		else cout << "non-symmetic" << endl;
+	
 	}
 	break;
 	case 5:
 	{
+		const int n = 10;
+		int a[n] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		
+		int b = 0;
 
+		for (int i = 0; i < n/2; i++) // данный цикл для "отражения" значений элементов. 
+		{                             // выполнять только до половины массива n/2, иначе просто придет к изначальным значениям если будет все до конца отражать
+			/*b = a[i];
+			a[i] = a[n - 1 - i];
+			a[n - 1 - i] = b;*/
+
+			swap(a[i], a[n - 1 - i]); // более оптимальная замена, без третьей переменной
+		}
+
+		for (int j = 0; j < n; j++) // отдельный ицкл нужен для вывода значений массива с ОТРАЖЕННЫМИ значениями
+		{
+			cout << a[j] << "\t";
+		}
+
+		cout << endl;
+		
 	}
 	break;
 	case 6:
